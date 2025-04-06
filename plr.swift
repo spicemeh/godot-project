2ee5d3c10d1b1739537e40b2795403066eee7090
spicemeh <99032431+spicemeh@users.noreply.github.com>
2025-04-06 15:04:05 +0300

    Update plr.swift

--- plr.swift
+++ plr.swift
@@ -21,12 +21,15 @@ var sensitivity = DEFAULT_SENSITIVITY
 var current_mode = speed_modifier.STANDING # standing, crouching, crawling
 
 func _ready() -> void:
+	cam.current = is_multiplayer_authority()
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 	DEFAULT_FOV = cam.fov # need to assign them here for some reason
 	ZOOM_FOV = DEFAULT_FOV - 50
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
 	if event is InputEventMouseMotion: # looking around
 		head.rotate_y(-event.relative.x * sensitivity)
 		cam.rotate_x(-event.relative.y * sensitivity)
@@ -41,24 +44,24 @@ func _input(event: InputEvent) -> void:
 		sensitivity = DEFAULT_SENSITIVITY
 
 func _physics_process(delta: float) -> void:
-	if is_multiplayer_authority():
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-		# Handle jump.
-		if Input.is_action_just_pressed("Jump") and is_on_floor() and current_mode == speed_modifier.STANDING:
-			velocity.y = JUMP_VELOCITY
-		
-		var speed = DEFAULT_SPEED + current_mode + (speed_modifier.SPRINTING if Input.is_action_pressed("Sprint") else 0)
-		
-		# Get the input direction and handle the movement/deceleration.
-		var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
-		var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
-		else:
-			velocity.x = 0
-			velocity.z = 0
-		
-		move_and_slide()
+	if not is_multiplayer_authority(): return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor() and current_mode == speed_modifier.STANDING:
+		velocity.y = JUMP_VELOCITY
+	
+	var speed = DEFAULT_SPEED + current_mode + (speed_modifier.SPRINTING if Input.is_action_pressed("Sprint") else 0)
+	
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = 0
+		velocity.z = 0
+	
+	move_and_slide()
